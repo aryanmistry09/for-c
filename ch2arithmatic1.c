7ea44b21927b9e74473c9eac8e2abf687d614c74
aryanmistry09 <aryanmistry00@gmail.com>
2025-01-23 23:44:19 +0530

    Add files via upload

---
+++ ch2arithmatic1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=-4;
+    int b=3;
+    int c=7;
+    int d=a+b+c;
+    printf("the value of a is %d and value of b is %d and sum c is %d and sum d is %d\n",a,b,c,d);
+    printf("the remainder when a is divided by b is =%d",a%b);
+return 0;
+}

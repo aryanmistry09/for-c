e17f10407ec549cfa343b291371ad8a6c6db0898
aryanmistry09 <aryanmistry00@gmail.com>
2025-01-26 17:38:30 +0530

    Add files via upload

---
+++ ch3problems3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int income;
+    float tax=0;
+    printf("Enter income = ");
+    scanf("%d",&income);
+    if (income<=250000)
+    {
+        tax=0;
+    }
+    else if (income>250000 && income<=500000)
+    {
+        tax=0.05 * (income - 250000);
+    }
+    else if (500000<income<=1000000)
+    {
+        tax=0.05 * (500000-250000) + 0.2 * (income - 500000);
+    }
+    else
+    {
+         tax=0.05 * (500000-250000) + 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
+    }
+    printf("The tax you need to pay is %f",tax);
+    return 0;
+}
